b2ca2a736baa487d5ac53a2928f6f455fb05ad23
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2022-09-04 19:42:37 -0400

    fix: routes

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -21,7 +21,7 @@ route.delete("/user", AuthMiddleware, DeleteUser);
 route.post("/movie", AuthMiddleware, CreateMovie);
 route.get("/movie/:id", AuthMiddleware, ReadMovie);
 route.get("/movies", AuthMiddleware, ReadMovies); //query example to paginate: ?page=2
-route.get("/movies", AuthMiddleware, ReadMoviesByUser); //query example to paginate: ?page=2
+route.get("/user/movies", AuthMiddleware, ReadMoviesByUser); //query example to paginate: ?page=2
 route.patch("/movie/:id", AuthMiddleware, UpdateMovie);
 route.delete("/movie/:id", AuthMiddleware, DeleteMovie);
 
